93b3709f8539476f073fcc3db7e80c116a571dcf
yen <yen.linkwang@nigao.re>
2019-10-03 14:12:40 +0400

    ap_hero/src/Migrations/Version2019: json to long text

--- ap_hero/src/Migrations/Version20191001113858.php
+++ ap_hero/src/Migrations/Version20191001113858.php
@@ -27,7 +27,7 @@ final class Version20191001113858 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_F0FE2527A76ED395 ON cart_item (user_id)');
         $this->addSql('ALTER TABLE product CHANGE picture_id picture_id INT DEFAULT NULL, CHANGE nutritionals_id nutritionals_id INT DEFAULT NULL, CHANGE category_id category_id INT DEFAULT NULL, CHANGE tva_id tva_id INT DEFAULT NULL, CHANGE price price DOUBLE PRECISION DEFAULT NULL');
         $this->addSql('ALTER TABLE nutritionals CHANGE k_cal k_cal DOUBLE PRECISION DEFAULT NULL');
-        $this->addSql('ALTER TABLE user CHANGE avatar_id avatar_id INT DEFAULT NULL, CHANGE roles roles JSON NOT NULL, CHANGE username username VARCHAR(60) DEFAULT NULL, CHANGE is_banned is_banned TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user CHANGE avatar_id avatar_id INT DEFAULT NULL, CHANGE roles roles LONGTEXT NOT NULL, CHANGE username username VARCHAR(60) DEFAULT NULL, CHANGE is_banned is_banned TINYINT(1) DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
