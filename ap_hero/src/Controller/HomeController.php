c0629fa078783d0bf2fd90c021c3f612ec36a27a
yen <yen.linkwang@nigao.re>
2019-10-03 17:47:39 +0400

    ap_hero/src: fixture test

--- ap_hero/src/Controller/HomeController.php
+++ ap_hero/src/Controller/HomeController.php
@@ -5,6 +5,12 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+//test purpose only
+//! Delete this part after pushing features
+use Faker\Factory;
+use Faker\Generator;
+use App\Entity\Product;
+
 class HomeController extends AbstractController
 {
     /**
@@ -12,6 +18,14 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $faker   = Factory::create();
+        $product = new Product();
+
+        $product->setName       ( $faker->company                                                );
+        $product->setDescription( $faker->sentence( $nbWords = 4, $variableNbWords = true )      );
+        $product->setPrice      ( $faker->randomFloat( $nbMaxDecimals = 2, $min = 5, $max = 15 ) );
+
+        dump( $product );
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
--- ap_hero/src/DataFixtures/ProductFixtures.php
+++ ap_hero/src/DataFixtures/ProductFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 class ProductFixtures extends Fixture
 {
+    protected $faker;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
+        $faker   = Factory::create();
+        $product = new Product();
+
+        $product->setName($faker->name);
+        $product->setDescription($faker->sentence($nbWords = 6, $variableNbWords = true));
+        dump($product);
+        
         // $manager->persist($product);
 
-        $manager->flush();
+        // $manager->flush();
     }
 }
