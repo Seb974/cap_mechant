3504422ec43e943953e8d4083b738788c2dad96c
yen <yen.linkwang@nigao.re>
2019-10-04 16:58:54 +0400

    a: allergen

---
+++ ap_hero/src/DataFixtures/AllergeneFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Allergen;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+
+		$zipcodes = array(
+			array('name' => 'gluten'),
+			array('name' => 'crustacés'),
+			array('name' => 'oeufs'),
+			array('name' => 'poissons'),
+			array('name' => 'arachides'),
+			array('name' => 'soja'),
+			array('name' => 'lait'),
+			array('name' => 'fruits à coques'),
+			array('name' => 'céleri'),
+			array('name' => 'moutarde'),
+			array('name' => 'graines de sésame'),
+			array('name' => 'Anhydride sulfureux et sulfites'),
+			array('name' => 'Lupin'),
+			array('name' => 'Mollusques'),
+		  );
+
+		foreach ( $Allergens as $key => $value ) {
+			$allergen = new Allergen();
+
+			$allergen->setName( $value['name'    ] );
+
+			$manager->persist($allergen);
+		}
+
+        $manager->flush();
+    }
+}
--- autorun.sh
+++ autorun.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ap_hero
+composer install
 php bin/console make:migration
 php bin/console doctrine:migrations:migrate
 php bin/console doctrine:schema:update --force
