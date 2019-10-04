4e05c32c72f19e0a734e35a3fab559e7fed8601f
yen <yen.linkwang@nigao.re>
2019-10-04 18:09:07 +0400

    autorun.sh:

--- autorun.sh
+++ autorun.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# change directory to symfony project
 cd ap_hero
+
+# force to install dependencies if some is missing
 composer install
-php bin/console doctrine:migrations:migrate
+
+# force database to update schema
 php bin/console doctrine:schema:update --force
+
+# load data samples
 php bin/console doctrine:fixtures:load
+
+# launch http server
 php bin/console server:start
+
+#launch a debugger server; in your controller use ```dump( $var );``` to obtain a var_dump
 php bin/console server:dump
