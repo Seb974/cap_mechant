ee3fe0fd85aab50b0cc91dd0cc2b3222835a4a96
Sébastien Maillot <sebastienmaillot@MacBook-Pro-de-Sebastien.local>
2019-10-04 23:37:29 +0400

    Correction de l'affectation des rôles

--- ap_hero/src/Form/UpdateUserType.php
+++ ap_hero/src/Form/UpdateUserType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use App\Form\CreateUserType;
 
 use Doctrine\ORM\EntityManagerInterface;
@@ -19,26 +20,20 @@ class UpdateUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {   
         $builder->remove('password');
-            
-            if ($options['role'] === 'ADMIN') {
-                $builder->add('roles', CheckboxType::class, [
-                              'label'    => 'Administrator',
-                              'data'     => true,
-                              'required' => false,
-                              'mapped'   => false,
-                ]);
-            } else {
-                $builder->add('roles', CheckboxType::class, [
-                              'label'    => 'Administrator',
-                              'data'     => false,
-                              'required' => false,
-                              'mapped'   => false,
-                ]);
-            }
-            $builder->add('isBanned', CheckboxType::class, [
-                          'label'    => 'Ban',
-                          'required' => false,
-                          'mapped'   => true,
+        $builder
+            ->add('roles', ChoiceType::class, [
+                'choices'  => [
+                    'user' => "ROLE_USER",
+                    'supplier' => "ROLE_SUPPLIER",
+                    'deliverer' => "ROLE_DELIVERER",
+                    'admin' => "ROLE_ADMIN",
+                ],
+                'mapped' => false,
+            ])
+            ->add('isBanned', CheckboxType::class, [
+                        'label'    => 'Ban',
+                        'required' => false,
+                        'mapped'   => true,
             ]);
     }
 }
