7fe03f4cf56e24f35571811aa6f1b784a600d332
yen <yen.linkwang@nigao.re>
2019-10-04 17:49:41 +0400

    autorun.sh:

--- autorun.sh
+++ autorun.sh
@@ -5,3 +5,5 @@ composer install
 php bin/console doctrine:migrations:migrate
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load
+php bin/console server:start
+php bin/console server:dump
