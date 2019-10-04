dac22b18404613e3073cbcbf3f8113d43051b667
yen <yen.linkwang@nigao.re>
2019-10-04 10:35:05 +0400

    ap_hero: composer install paiment

--- ap_hero/src/Controller/HomeController.php
+++ ap_hero/src/Controller/HomeController.php
@@ -18,15 +18,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        $faker = Factory::create();
-        $faker->addProvider( new \FakerRestaurant\Provider\fr_FR\Restaurant( $faker ) );
-
-        $product = new Product();
-        $product->setName       ( $faker->foodName() );
-        $product->setDescription( $faker->sentence( $nbWords = 4, $variableNbWords = true )      );
-        $product->setPrice      ( $faker->randomFloat( $nbMaxDecimals = 2, $min = 5, $max = 15 ) );
-
-        dump( $product );
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
