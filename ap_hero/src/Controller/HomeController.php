78588576b7b0f2b18e23c4da8fa951c3e58d8cda
yen <yen.linkwang@nigao.re>
2019-10-07 11:37:34 +0400

    ap_hero:

--- ap_hero/src/Controller/HomeController.php
+++ ap_hero/src/Controller/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Variant;
+use App\Form\VariantType;
 use App\Repository\VariantRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
