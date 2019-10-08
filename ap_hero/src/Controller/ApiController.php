909ad6782a130bdd8f9871665669d3df29c604d2
yen <yen.linkwang@nigao.re>
2019-10-08 16:32:38 +0400

    ap_hero:

--- ap_hero/src/Controller/ApiController.php
+++ ap_hero/src/Controller/ApiController.php
@@ -2,10 +2,21 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use FOS\UserBundle\Model\UserManagerInterface;
+use KnpU\OAuth2ClientBundle\Security\Authenticator\SocialAuthenticator;
+use KnpU\OAuth2ClientBundle\Client\Provider\FacebookClient;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use League\OAuth2\Client\Provider\FacebookUser;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class ApiController extends AbstractController
 {
@@ -22,11 +33,17 @@ class ApiController extends AbstractController
 	/**
      * @Route("/api/callback", name="api_callback")
      */
-    public function callback( Request $request ): Response
+    public function callback( Request $request, ClientRegistry $clientRegistry ): Response
     {
+		$facebookclient = $clientRegistry->getClient('facebook');
+		$facebookUser   = new FacebookUser( $request->query );
+		// $parameters[] = $request->query->get("code");
+		// $parameters = array();
+		// = = $facebookclient->fetchUserFromToken($token);
+
         return $this->render('api/index.html.twig', [
 			'controller_name' => 'ApiController',
-			'request' => $request
+			'request' => $facebookUser
         ]);
     }
 }
