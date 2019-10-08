ec2f8a23af6dd5c49164ed8cb6e8fb52f77563fc
yen <yen.linkwang@nigao.re>
2019-10-08 17:05:05 +0400

    ap_hero/src/Controller/ApiController.php:

--- ap_hero/src/Controller/ApiController.php
+++ ap_hero/src/Controller/ApiController.php
@@ -10,6 +10,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use League\OAuth2\Client\Token\AccessToken;
+
 class ApiController extends AbstractController
 {
     /**
@@ -25,15 +27,20 @@ class ApiController extends AbstractController
 	/**
      * @Route("/api/callback", name="api_callback")
      */
-    public function callback( Request $request, ClientRegistry $clientRegistry, EntityManagerInterface $em, UserManagerInterface $userManager ): Response
+    public function callback( Request $request, ClientRegistry $clientRegistry ): Response
     {
-		// $parameters[] = $request->query->get("code");
+		$parameters['code']  = $request->query->get("code");
+		$parameters['state'] = $request->query->get("state");
+		$accessToken         = new AccessToken( array( 'access_token' => $parameters['code'] ) );
+		$facebookClient      = $clientRegistry->getClient('facebook');
+		$facebookUser        = $facebookClient->fetchUserFromToken( $accessToken );
+
 		// $parameters = array();
-		// = = $facebookclient->fetchUserFromToken($token);
+		// = = $facebookclient->fetchUserFromToken($accessToken);
 
         return $this->render('api/index.html.twig', [
 			'controller_name' => 'ApiController',
-			'request' => 'toto'
+			'request' => $facebookUser
         ]);
     }
 }
