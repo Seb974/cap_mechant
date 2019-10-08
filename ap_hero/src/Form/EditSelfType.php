7e3a3e53103b242d46b2d39c6fad9432b74f5e5c
Vitry <anne-marion119@hotmail.com>
2019-10-08 12:08:21 +0400

    Edit self, show self et register 100% avec delivery address et billing address

--- ap_hero/src/Form/EditSelfType.php
+++ ap_hero/src/Form/EditSelfType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use App\Entity\City;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
