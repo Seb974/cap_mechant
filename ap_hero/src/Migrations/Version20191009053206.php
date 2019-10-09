5a385f203de9470e4b7a0b984be6f34f97bb2675
yen <yen.linkwang@nigao.re>
2019-10-09 11:20:53 +0400

    ap_hero/src/Migrations/Version20191009053206.php: move

--- ap_hero/src/Migrations/Version20191009053206.php
+++ ap_hero/src/Migrations/Version20191009053206.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191008103142 extends AbstractMigration
+final class Version20191009053206 extends AbstractMigration
 {
     public function getDescription() : string
     {
