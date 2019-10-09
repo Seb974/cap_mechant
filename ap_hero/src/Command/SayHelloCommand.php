83bd47117ff69df06f9280d897c2cc5acfe6577d
yen <yen.linkwang@nigao.re>
2019-10-09 16:32:40 +0400

    ap_hero/src/Command/SayHelloCommand.php:

--- ap_hero/src/Command/SayHelloCommand.php
+++ ap_hero/src/Command/SayHelloCommand.php
@@ -27,7 +27,7 @@ class SayHelloCommand extends Command
 		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 		curl_setopt( $ch, CURLOPT_URL, $url );
 		$result = curl_exec( $ch );
-		$output->writeln( $url );
+		$output->writeln( $result );
 	}
 
 }
