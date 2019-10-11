c92e1e9cefc7bf4f2fa59f9957a8a1bcd19203fd
Vitry <anne-marion119@hotmail.com>
2019-10-11 12:36:24 +0400

    Jouer un peu avec deliver

--- ap_hero/src/Controller/DelivererController.php
+++ ap_hero/src/Controller/DelivererController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Orders;
+
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,38 +14,54 @@ class DelivererController extends AbstractController
     /**
      * @Route("/deliverer", name="deliverer")
      */
-    public function index()
+    public function index(UserRepository $userRepository)
     {
-        return $this->render('deliverer/index.html.twig', [
-            'controller_name' => 'DelivererController',
-        ]);
-	}
+		$user = $this->getUser();
+		$deliverers = [];
+        foreach ($userRepository->findAll() as $del) {
+            $deliverRole = (in_array('ROLE_DELIVERER', $del->getRoles())) ? 'ROLE_DELIVERER' : false;
+            if ($deliverRole) {
+                array_push($deliverers, $del);
+			}
+			dump($deliverers);
+        }
 
-	/**
+        // if ($deliverer === 'ROLE_ADMIN') {
+        //     return $this->render('stock/index.html.twig', [
+        //         'stocks' => $allStock,
+        //     ]);
+        // }
+
+        return $this->render('deliverer/index.html.twig', [
+            'deliverers' => $deliverers,
+        ]);
+    }
+
+    /**
      * @Route("/deliverer/cron", name="deliverer_cron")
      */
     public function cronUpdate( EntityManagerInterface $em )
     {
-		$now = new \DateTime();
-		$orders = $em->getRepository( Orders::class )->findAll();
-		$answer = "no orders";
-		foreach ($orders as $key => $order) {
-			$orderPayedTime = $order->getPayDateTime();
+        $now = new \DateTime();
+        $orders = $em->getRepository( Orders::class )->findAll();
+        $answer = "no orders";
+        foreach ($orders as $key => $order) {
+            $orderPayedTime = $order->getPayDateTime();
 
-			$supplierTimer_hr = $order->getSupplier()->getPreparationPeriod()->format('H');
-			$supplierTimer_mn = $order->getSupplier()->getPreparationPeriod()->format('i');
-			$timer = new \DateInterval( "PT{$supplierTimer_hr}H{$supplierTimer_mn}M" );
-			$checkDelay = $orderPayedTime->add( $timer );
-			if ( $checkDelay > $now ) {
-				$answer = "allez livrer";
-			} else {
-				$answer = "reste la case";
-			}
-		}
+            $supplierTimer_hr = $order->getSupplier()->getPreparationPeriod()->format('H');
+            $supplierTimer_mn = $order->getSupplier()->getPreparationPeriod()->format('i');
+            $timer = new \DateInterval( "PT{$supplierTimer_hr}H{$supplierTimer_mn}M" );
+            $checkDelay = $orderPayedTime->add( $timer );
+            if ( $checkDelay > $now ) {
+                $answer = "allez livrer";
+            } else {
+                $answer = "reste la case";
+            }
+        }
 
         return $this->render('deliverer/index.html.twig', [
-			'controller_name' => 'DelivererController',
-			'answer' => $answer
+            'controller_name' => 'DelivererController',
+            'answer' => $answer
         ]);
     }
 }
