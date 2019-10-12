72cf18ecd16457ca140a341319c80529612985a0
yen <yen.linkwang@nigao.re>
2019-10-12 23:15:27 +0400

    ap_hero/Security/AppAuthenticator:

--- ap_hero/src/Security/AppAuthenticator.php
+++ ap_hero/src/Security/AppAuthenticator.php
@@ -71,7 +71,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException("Erreur lors de la connexion.");
         }
 
         return $user;
